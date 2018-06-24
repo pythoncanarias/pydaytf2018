b3e49b83903c016c88d51d6f7ea0ff2311baaad9
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-06-24 20:04:55 +0100

    Avoid permissions errors when creating public content

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 git push
-ssh micropython.pythoncanarias.es "cd ~/pydaytf2018; git pull; npm run build"
+ssh micropython.pythoncanarias.es "
+    cd ~/pydaytf2018;
+    git pull;
+    rm -fr public/*;
+    npm run build"
